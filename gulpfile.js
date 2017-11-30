aef66ecf30b5cc46d5ada71bb95a91b9f3d9aecd
kikers17 <luisenripabe@hotmail.com>
2017-11-30 08:29:16 -0500

    postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
 var browserSync = require('browser-sync').create();
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
+var atImport = require("postcss-import");
+var cssnext = require('postcss-cssnext');
+
+// Servidor de desarrollo
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+})
+
+// Tarea para procesar el CSS
+
+gulp.task('css', function () {
+
+  var processors = [
+  		cssnext(),
+  		atImport(),
 
 
+  ]
+
+  gulp.src('src/css/menu.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
+})
 
 
+// Tarea para vigilar los cambios
+gulp.task('watch', function () {
+  gulp.watch('src/css/*.css', ['css']).on('change', browserSync.reload)
+  gulp.watch('*.html').on('change', browserSync.reload)
+})
 
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
-
-
-gulp.task('default', ['browser-sync']);
-
+gulp.task('default', ['watch', 'css' , 'serve'])
